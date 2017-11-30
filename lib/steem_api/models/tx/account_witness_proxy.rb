fa995b1482558211f9593d6e9a69d1f602ee2131
Anthony Martin <github@martin-studio.com>
2017-11-30 14:04:23 -0800

    Added :active scope.  Lets you find actively proxied accounts (as opposed to all accounts that are currently and at one time proxied).

--- lib/steem_api/models/tx/account_witness_proxy.rb
+++ lib/steem_api/models/tx/account_witness_proxy.rb
@@ -3,7 +3,19 @@ module SteemApi
     class AccountWitnessProxy < SteemApi::SqlBase
 
       self.table_name = :TxAccountWitnessProxies
-
+      
+      scope :active, lambda { |account = nil|
+        active = all
+        active = active.where(proxy: account) if !!account
+        
+        ids = active.map do |p|
+          p.id unless AccountWitnessProxy.where(account: p.account).
+            where.not(proxy: p.Proxy).
+            where('timestamp > ?', p.timestamp).exists?
+        end
+        
+        where(id: ids)
+      }
     end
   end
 end
